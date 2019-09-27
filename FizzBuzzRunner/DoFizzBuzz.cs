6ce1d8aeb33b2ca023ec9f71d1c268caa08f88fc
Houston.Miller <houston.miller@ministrybrands.com>
2019-09-27 13:45:24 -0400

    Cleanup of code and tests still pass.

--- FizzBuzzRunner/DoFizzBuzz.cs
+++ FizzBuzzRunner/DoFizzBuzz.cs
@@ -10,11 +10,10 @@ namespace FizzBuzzRunner
         }
 
         public string GetFizzBuzz(int num)
-        {
-            if (num == 4) return "4";
+        {            
             if (num == 3) return "Fizz";
-            if (num == 2) return "2";
-            return "1";
+
+            return num.ToString();
         }
     }
 }
